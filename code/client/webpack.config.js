d8a0094a299d283e1bcb33bbcad33829e95e8cad
zhuzhen.bk <zhuzhen.bk@alibaba-inc.com>
2020-05-27 10:39:07 +0800

    add - precache files

--- code/client/webpack.config.js
+++ code/client/webpack.config.js
@@ -36,16 +36,14 @@ module.exports = {
          template: path.resolve( __dirname, './src/index.html' ),
          filename: 'index.html'
       }),
-
-      new GenerateSW({
-        // these options encourage the ServiceWorkers to get in there fast
-        // and not allow any straggling "old" SWs to hang around
-        clientsClaim: true,
-        skipWaiting: true,
+      new InjectManifest({
+        swSrc: './src/sw.js'
       }),
-    //   new InjectManifest({
-    //     swSrc: './src/sw.js',
-    //     swDest: 'sw.js'
-    //   })
+      // new GenerateSW({
+      //   // these options encourage the ServiceWorkers to get in there fast
+      //   // and not allow any straggling "old" SWs to hang around
+      //   clientsClaim: true,
+      //   skipWaiting: true,
+      // }),
    ]
 };
